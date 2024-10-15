915dfa4082a5b2d9c679a6f8c4bb4a4e08b19fb6
Ioana <imatei@opentext.com>
2024-10-15 12:11:11 +0300

    test console

--- src/test/java/com/opentext/mada/GraderTest.java
+++ src/test/java/com/opentext/mada/GraderTest.java
@@ -16,12 +16,14 @@ public class GraderTest {
     @Test
    public void fiftyNineShouldReturnF () {
         Grader grader = new Grader();
+        System.out.println("-------------------fiftyNineShouldReturnF--------------------------");
         assertEquals('F',grader.determineLetterGrade(59));
     }
 
     @Test
     public void sixtyNineShouldReturnD () {
         Grader grader = new Grader();
+        System.out.println("-------------------sixtyNineShouldReturnD--------------------------");
         assertEquals('D',grader.determineLetterGrade(69));
     }
 
@@ -29,24 +31,28 @@ public class GraderTest {
     @Disabled
     public void seventyNineShouldReturnC () {
         Grader grader = new Grader();
+        System.out.println("-------------------seventyNineShouldReturnC--------------------------");
         assertEquals('C',grader.determineLetterGrade(79));
     }
 
     @Test
     public void eightyNineShouldReturnB () {
         Grader grader = new Grader();
+        System.out.println("-------------------eightyNineShouldReturnB--------------------------");
         assertEquals('B',grader.determineLetterGrade(89));
     }
 
     @Test
     public void ninetyNineShouldReturnA () {
         Grader grader = new Grader();
+        System.out.println("-------------------ninetyNineShouldReturnA--------------------------");
         assertEquals('A',grader.determineLetterGrade(99));
     }
 
     @Test
     public void eightyShouldReturnB () {
         Grader grader = new Grader();
+        System.out.println("-------------------eightyShouldReturnB--------------------------");
         assertEquals('B',grader.determineLetterGrade(80));
     }
 
@@ -60,12 +66,14 @@ public class GraderTest {
     @Test
     public void zOneHundredShouldReturnA () {
         Grader grader = new Grader();
+        System.out.println("-------------------zOneHundredShouldReturnA--------------------------");
         assertEquals('A',grader.determineLetterGrade(100));
     }
 
     @Test
     public void negativeOneShouldReturnIllegalArgumentException() {
         Grader grader = new Grader();
+        System.out.println("-------------------zOneHundredShouldReturnA--------------------------");
         assertThrows(IllegalArgumentException.class, () -> {
             grader.determineLetterGrade(-1);
         });
@@ -81,18 +89,21 @@ public class GraderTest {
     @Test
     public void environmentCase() {
         Grader grader = new Grader();
+        System.out.println("-------------------environmentCase--------------------------");
         System.out.println(System.getProperty("ENV"));
     }
 
     @Test
     public void $data_Driven_Properties() {
         Grader grader = new Grader();
+        System.out.println("-------------------$data_Driven_Properties--------------------------");
         System.out.println(System.getProperty("DataSet1"));
     }
 
     @Test
     public void _predefinedParameters() {
         Grader grader = new Grader();
+        System.out.println("-------------------_predefinedParameters--------------------------");
         System.out.println(System.getProperty("#sctm_keywords"));
         System.out.println(System.getProperty("SILK Test Name"));
 
@@ -101,6 +112,7 @@ public class GraderTest {
     @Test
     public void systemParameters() {
         Grader grader = new Grader();
+        System.out.println("-------------------systemParameters--------------------------");
         System.out.println(System.getProperty("#sctm_execdef_name"));
         System.out.println(System.getProperty("#sctm_product"));
         System.out.println(System.getProperty("#sctm_test_name"));
