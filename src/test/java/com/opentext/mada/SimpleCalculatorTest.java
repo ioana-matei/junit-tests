cadd300e9796d9e0530d16caf4ebe33ba23d451b
Ioana <imatei@opentext.com>
2024-10-22 12:01:55 +0300

    removing some tests for pipeline runs

--- src/test/java/com/opentext/mada/SimpleCalculatorTest.java
+++ src/test/java/com/opentext/mada/SimpleCalculatorTest.java
@@ -1,25 +1,25 @@
-package com.opentext.mada;
-
-
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-public class SimpleCalculatorTest {
-
-    @Test
-   public void twoPlusTwoShouldEqualFour() {
-        SimpleCalculator calculator = new SimpleCalculator();
-        assertEquals(4, calculator.add(2,2));
-        assertTrue(calculator.add(2, 2) == 4);
-
-    }
-
-    @Test
-   public void threePlusSevenShouldEqualTen() {
-        SimpleCalculator calculator = new SimpleCalculator();
-        assertEquals(10, calculator.add(3,7));
-
-    }
-
-}
+//package com.opentext.mada;
+//
+//
+//import org.junit.jupiter.api.Test;
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.junit.jupiter.api.Assertions.assertTrue;
+//
+//public class SimpleCalculatorTest {
+//
+//    @Test
+//   public void twoPlusTwoShouldEqualFour() {
+//        SimpleCalculator calculator = new SimpleCalculator();
+//        assertEquals(4, calculator.add(2,2));
+//        assertTrue(calculator.add(2, 2) == 4);
+//
+//    }
+//
+//    @Test
+//   public void threePlusSevenShouldEqualTen() {
+//        SimpleCalculator calculator = new SimpleCalculator();
+//        assertEquals(10, calculator.add(3,7));
+//
+//    }
+//
+//}
--- src/test/java/com/opentext/mada/SquareRootCalculatorTest.java
+++ src/test/java/com/opentext/mada/SquareRootCalculatorTest.java
@@ -1,15 +1,15 @@
-package com.opentext.mada;
-
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-public class SquareRootCalculatorTest {
-
-    @Test
-    public void SixteenSqrtShouldBeFour() {
-        SquareRootCalculator sqrtcalculator = new SquareRootCalculator();
-        System.out.println("-------------------SixteenSqrtShouldBeFour--------------------------");
-        assertEquals(4,sqrtcalculator.squareRoot(16));
-    }
-}
+//package com.opentext.mada;
+//
+//import org.junit.jupiter.api.Test;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//public class SquareRootCalculatorTest {
+//
+//    @Test
+//    public void SixteenSqrtShouldBeFour() {
+//        SquareRootCalculator sqrtcalculator = new SquareRootCalculator();
+//        System.out.println("-------------------SixteenSqrtShouldBeFour--------------------------");
+//        assertEquals(4,sqrtcalculator.squareRoot(16));
+//    }
+//}
